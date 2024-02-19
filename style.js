49e85f92872fde98190618df2a508e4608341b14
Akula Anil Kumar <157016471+Anilroy30@users.noreply.github.com>
2024-02-19 08:37:42 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,239 @@
+@import url('https://fonts.googleapis.com/css2?family=Bree+Serif&family=Caveat:wght@400;700&family=Lobster&family=Monoton&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Playfair+Display+SC:ital,wght@0,400;0,700;1,700&family=Playfair+Display:ital,wght@0,400;0,700;1,700&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&family=Source+Sans+Pro:ital,wght@0,400;0,700;1,700&family=Work+Sans:ital,wght@0,400;0,700;1,700&display=swap');
+
+.icon {
+    height: 10vh;
+    width: 37vw;
+}
+
+.main-head {
+    color: #183b56;
+    font-family: "Roboto";
+    font-size: 34px;
+    font-weight: 700;
+}
+
+.main-para {
+    color: #616c76;
+    font-family: "Roboto";
+    font-size: 17px;
+    font-weight: 400;
+}
+
+.short-head {
+    color: #1f2933;
+    font-size: 18px;
+    font-weight: 500;
+}
+
+.short-para {
+    color: #7b8794;
+    font-size: 13px;
+}
+
+.main-head2 {
+    color: #183b56;
+    font-family: "Roboto";
+    font-weight: 700;
+    font-size: 30px;
+}
+
+.modal-head {
+    font-size: 16px;
+}
+
+.bg-container {
+    background-image: url("https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/ecommerce-products-headphones-bg.png");
+    background-size: cover;
+    border-radius: 8px;
+    height: 100%;
+}
+
+.bg-container1 {
+    background-image: url("https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/ecommerce-products-laptops-bg.png");
+    background-size: cover;
+    border-radius: 8px;
+    height: 100%;
+}
+
+.short-head2 {
+    color: white;
+    font-family: "Roboto";
+    font-weight: 600;
+}
+
+.short-para2 {
+    color: white;
+    font-family: "Roboto";
+    font-size: 16px;
+}
+
+.custom-button {
+    background-color: white;
+    color: #ff9f00;
+    height: 8vh;
+    width: 35vw;
+    border-width: 0px;
+    border-radius: 20px;
+}
+
+.custom-anchor {
+    color: #ff9f00;
+    font-size: 20px;
+    font-weight: bold;
+}
+
+.modal-para {
+    font-size: 13px;
+}
+
+.buy-button {
+    border-style: solid;
+    border-color: #323f4b;
+    border-width: 1px;
+}
+
+span {
+    color: #323f4b;
+    font-weight: bold;
+}
+
+.bg-container2 {
+    background-image: linear-gradient(to right, #f49541, #dd5d97, #ad2167);
+    background-size: cover;
+}
+
+.main-head3 {
+    color: #ad2167;
+    font-size: 27px;
+    font-family: "Bree serif";
+}
+
+.main-para3 {
+    color: #ffffff;
+    font-size: 18px;
+    font-family: "roboto";
+}
+
+.span {
+    font-weight: bold;
+    font-style: italic;
+}
+
+.main-head4 {
+    color: #183b56;
+    font-size: 40px;
+    font-family: "Roboto";
+    font-weight: 700;
+}
+
+.short-para4 {
+    color: #959ead;
+    font-size: 16px;
+}
+
+.extra4 {
+    font-size: 20px;
+}
+
+.short-head4 {
+    font-size: 21px;
+}
+
+.custom-button4 {
+    color: #ff9f00;
+    font-size: 15px;
+    font-weight: bold;
+}
+
+.bg-container5 {
+    background-color: #f5f7fa;
+    background-size: cover;
+}
+
+.main-head5 {
+    color: #183b56;
+    font-size: 40px;
+    font-weight: 700;
+    font-family: "Roboto";
+}
+
+.extra5 {
+    font-size: 22px;
+}
+
+.extra6 {
+    border-radius: 10px;
+}
+
+.short-para5 {
+    color: #1f2933;
+    font-size: 15px;
+    font-weight: 500;
+}
+
+.custom-anchor5 {
+    color: #ff9f00;
+    font-size: 16px;
+    font-weight: bold;
+}
+
+.bg-container7 {
+    background-color: #f5f7fa;
+    background-size: cover;
+}
+
+.main-head7 {
+    color: #183b56;
+    font-family: "Roboto";
+    font-size: 26px;
+    font-weight: 600;
+}
+
+.custom-button7 {
+    background-color: transparent;
+    height: 8vh;
+    width: 38vw;
+    border-style: solid;
+    border-color: #ff9f00;
+    color: #ff9f00;
+}
+
+.bg-container8 {
+    background-color: #0d2436;
+    background-size: cover;
+}
+
+.bg-container9 {
+    background-color: #152c3e;
+    border-radius: 40px;
+    height: 45px;
+    width: 45px;
+    padding: 14px;
+    margin-right: 16px;
+}
+
+.icon8 {
+    color: #5a7184;
+    font-size: 20px;
+}
+
+.list8 {
+    list-style-type: none;
+    padding-left: 0px;
+}
+
+.line8 {
+    background-color: #5a7184;
+    border-width: 2px;
+}
+
+.main-para8 {
+    color: #5a7184;
+    font-family: "Roboto";
+    font-size: 15px;
+}
+
+.extra8 {
+    font-size: 17px;
+    font-weight: 600;
+}
